4c4b35d17291e74132cc7f4ad502b36f15737850
N1haizm <nihat.sh10@gmail.com>
2024-09-17 14:10:30 +0400

    nth commit

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,12 @@
+String showFunc(int a, int b){
+  return "Add ${a} + ${b} = ${add(a,b)}\nSub ${a} - ${b} = ${sub(a,b)}";
+
+}
+
+int add(int a, int b){
+  return a + b;
+}
+
+int sub(int a, int b){
+  return a-b;
+}
